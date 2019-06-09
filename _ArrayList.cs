5e0de141b974eacd92800dfec1d24360c28139e4
atakan erturk <36862019+atakanertrk@users.noreply.github.com>
2019-06-09 18:23:26 +0300

    Add files via upload

---
+++ _ArrayList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayList_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arraylist list = new Arraylist();
+            list.ListeyeEkleInt(5);
+            list.ListeyeEkleInt(10);
+
+            list.ListeyeEkleStr("BLUE");
+            list.ListeyeEkleStr("RED");
+
+            list.ListeGoruntule();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Arraylist
+    {
+        ArrayList values = new ArrayList();
+
+        public void ListeyeEkleInt(int x)
+        {
+            values.Add(x);
+        }
+        public void ListeyeEkleStr(string y)
+        {
+            values.Add(y);
+        }
+        public void ListeGoruntule()
+        {
+            foreach(var values in values)
+            {
+                Console.WriteLine(values);
+            }
+        }
+    }
+}
