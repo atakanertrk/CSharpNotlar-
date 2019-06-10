db9f49b7968731660908132f9353f52f5b76d565
atakan erturk <36862019+atakanertrk@users.noreply.github.com>
2019-06-10 11:22:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ExceptionIntro();
+            try
+            {
+                Find();
+            }
+            catch(RecordNotFoundException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            
+            Console.ReadLine();
+        }
+
+        //private static void ExceptionIntro()
+        //{
+        //    try
+        //    {
+        //        string[] students = new string[3] { "engin", "derin", "salih" };
+        //        students[3] = "ahmet";
+        //    }
+        //    catch(IndexOutOfRangeException exception)
+        //    {
+        //        Console.WriteLine(exception.Message);
+        //    }
+        //}
+        private static void Find()
+        {
+            List<string> students = new List<string> { "engin", "derin", "salih" };
+            if (!students.Contains("ahmet"))
+            {
+                throw new RecordNotFoundException("Record Not Found");
+            }
+            else
+            {
+                Console.WriteLine("record found");
+            }
+        }
+        
+    }
+
+}
