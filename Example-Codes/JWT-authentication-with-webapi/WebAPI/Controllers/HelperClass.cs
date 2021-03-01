d442e9c697693af7a8038a6b44e5c7bfbea8da3a
atakan <atakanertrk@hotmail.com>
2021-03-01 13:44:50 +0300

    jwt web api example

---
+++ Example-Codes/JWT-authentication-with-webapi/WebAPI/Controllers/HelperClass.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI
+{
+    public class HelperClass
+    {
+        IConfiguration _config;
+        public HelperClass(IConfiguration config)
+        {
+            _config = config;
+        }
+        /// <summary>
+        /// returns LoginModel if user name is atakan and password is 123 (for demo purposes), 
+        /// otherwise returns null LoginModel object
+        /// </summary>
+        public LoginModel AuthenticateUser(LoginModel login)
+        {
+            LoginModel user = null;
+            if (login.Name == "atakan" && login.Password == "123")
+            {
+                user = new LoginModel { Name = "_atakan", Password = "_123" };
+            }
+            return user;
+        }
+
+        public string GenerateJSONWebToken(LoginModel userinfo)
+        {
+            var securityKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            // with securityKey, we create secure tokens which no one can generate without knowing the "Key"
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                // Writing login information inside toke is not safe, its better to write userId in token.
+                new Claim(JwtRegisteredClaimNames.Sub,userinfo.Name),
+                new Claim(JwtRegisteredClaimNames.Email,userinfo.Password),
+                // Guid generates unique string
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+            };
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.UtcNow.AddMinutes(5),
+                signingCredentials: credentials);
+            var encodetoken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodetoken;
+        }
+    }
+}
---
+++ Example-Codes/JWT-authentication-with-webapi/WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        private HelperClass _helper;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+            _helper = new HelperClass(config);
+        }
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginModel loginValue)
+        {
+            LoginModel login = new LoginModel();
+            login.Name = loginValue.Name;
+            login.Password = loginValue.Password;
+
+            IActionResult response = Unauthorized();
+
+            var user = _helper.AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenStr = _helper.GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
+
+            return response;
+        }
+
+        [Authorize]
+        [HttpGet]
+        // requires JWT token send via header or authorization jwt bearer
+        public string WhoIsThis()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IList<Claim> claim = identity.Claims.ToList();
+            var userName = claim[0].Value;
+            var password = claim[1].Value;
+            return "Welcome " + userName + " your password is: " + password;
+        }
+    }
+}
